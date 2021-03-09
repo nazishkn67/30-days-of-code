10a321cd5efe59cb3d4d452e4d3e8cfc8e1ec257
nazishkn67 <40017618+nazishkn67@users.noreply.github.com>
2021-03-09 15:42:25 +0530

    Update Day 02 - DataTypes.java

--- Day 02 - DataTypes.java
+++ Day 02 - DataTypes.java
@@ -8,15 +8,17 @@ public class DataTypes {
 		
         Scanner scan = new Scanner(System.in);     //creates scanner object to read input from stdin
       
-        int i1 = scan.nextInt();                   // reads input
-        double d1 = scan.nextDouble();                     //from stdin 
+        int i1 = scan.nextInt();
+	    
+        double d1 = scan.nextDouble();            // reads input from stdin 
         scan.nextLine();
+	    
         String s1 = scan.nextLine();
         
-        System.out.println(i1+i2);           //prints sum of 1st integer and 2nd integer
-		    System.out.println(d1+d2);
-        System.out.println(s1+s2);           //prints concatenation of 1st string and 2nd string 
+        System.out.println(i1+i2);          	 //prints sum of 1st integer and 2nd integer
+	System.out.println(d1+d2);
+        System.out.println(s1+s2);           	//prints concatenation of 1st string and 2nd string 
         
-        scan.close();                        //closes scanner object
+        scan.close();                        	//closes scanner object
     }
 }
