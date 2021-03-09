5304552d9472bf17027539e9318620ccccbc2034
nazishkn67 <40017618+nazishkn67@users.noreply.github.com>
2021-03-09 14:39:41 +0530

    file created

---
+++ Hello World.java
@@ -0,0 +1,12 @@
+
+public class Solution {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);   //creates scanner object to read input from stdin
+		String inputString = scan.nextLine();    //read string input and save to variable "inputString"
+		scan.close();                            //closes scanner object
+      
+		System.out.println("Hello, World.");     //prints Hello, World. as output 
+    System.out.println(inputString);         //prints inputString variable
+	}
+}
