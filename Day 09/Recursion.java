fb8dc83259065d4b94364daaeefc9cf0ca7915e6
Nazish Kamran <40017618+nazishkn67@users.noreply.github.com>
2024-04-30 16:42:29 +0530

    Update Recursion.java

--- Day 09/Recursion.java
+++ Day 09/Recursion.java
@@ -1,37 +1,22 @@
 import java.io.*;
+import java.util.*;
 
 class Result {
-
-    /*
-     * Complete the 'factorial' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER n as parameter.
-     */
-
     public static int factorial(int n) {
-    // Write your code here
-    if(n==1 || n==0)
-        return 1;
-    else
-        return n * factorial(n-1);
+        if(n <= 1)     //base case
+            return 1;
+        else           //recursive case
+            return n * factorial(n-1);
     }
-
 }
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        Scanner sc = new Scanner(System.in);    //creates scanner object
+        int n = sc.nextInt();                   //takes integer input
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-        int result = Result.factorial(n);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        int result = Result.factorial(n);       //calling recursive function
+        System.out.println(result);             //printing result to the console
+        sc.close();                             //closes scanner object
     }
 }
